34199a30701ac4b11aa7d2b233708f9a7f5feb05
Robin Chaudhary <torino2019>
2020-05-23 22:17:07 +0200

    Schema files generated and test case updated.

--- MyTheresa/src/test/java/ApiTest/TechnicalChallenge2.java
+++ MyTheresa/src/test/java/ApiTest/TechnicalChallenge2.java
@@ -1,11 +1,17 @@
 package ApiTest;
+import java.util.List;
+
+import org.apache.http.params.CoreConnectionPNames;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import io.restassured.RestAssured;
+import io.restassured.config.HttpClientConfig;
+import io.restassured.config.RestAssuredConfig;
 import io.restassured.http.Header;
 import io.restassured.http.Headers;
 import io.restassured.http.Method;
+import io.restassured.mapper.ObjectMapperType;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
@@ -17,6 +23,7 @@ public class TechnicalChallenge2 {
 	
 	@BeforeTest
 	public void setup() {
+		
 		// specify base URI
 		RestAssured.baseURI = "https://api.github.com/";
 
@@ -24,51 +31,55 @@ public class TechnicalChallenge2 {
 		RequestSpecification httpRequest = RestAssured.given();
 				
 		// define HTTP mehtod. 
-		response = httpRequest.request(Method.GET, "/search/repositories?q=tetris+language:assemb ly&sort=stars&order=desc");
-
+		response = httpRequest.request(Method.GET, "/search/repositories?q=tetris+language:assembly&sort=stars&order=desc");
+	
 	}
 	
 	@Test(description = "status code validation", priority = 1)
-	public void statusCodeValidation() {
+	public void statusCodeValidation() throws InterruptedException {
 		int statusCode = response.getStatusCode();
 		System.out.println("Status code is: " + +statusCode);
 		Assert.assertEquals(statusCode, 200);
 	}
 	
 	@Test(description = "status line validation", priority = 2)
-	public void statusLineValidation() {
+	public void statusLineValidation() throws InterruptedException {
 		String statusLine = response.getStatusLine();
 		System.out.println("Status line is: " + statusLine);
 		Assert.assertEquals(statusLine, "HTTP/1.1 200 OK");
 	}
 	
-	@Test(description = "response Body print on console", priority = 3)
-	public void responseBody() {
-		String responseBody = response.getBody().asString();
-		System.out.println("Response body is: " + responseBody);
+	@Test(description = "From response body schema, verify all id field have values", priority = 3)
+	public void responseBody() throws InterruptedException {
+		Schema schema = response.as(Schema.class, ObjectMapperType.GSON);
+		List<Item> items = schema.getItems();
+		for(Item item : items) {
+			Assert.assertTrue("Id is Blank", !item.getId().equals(""));
+			Thread.sleep(100);
+		}
 	}
 	
 	@Test(description = "header key value Validation", priority = 4)
-	public void header() {
+	public void header() throws InterruptedException {
 		String contentType = response.header("Content-Type");
 		System.out.println("Content Type is : " + contentType);
 		Assert.assertEquals(contentType, "application/json; charset=utf-8");
 	}
 	
 	@Test(description = "validate json attribute values", priority = 5)
-	public void jsonAttributes() {
+	public void jsonAttributes() throws InterruptedException {
 		JsonPath jsonPath = response.jsonPath();
-		int totalCount = jsonPath.get("total_count");
-		System.out.println("Total Count is : " + totalCount);
-		Assert.assertEquals(3, totalCount);
+		boolean jsonAttributeValue = jsonPath.get("incomplete_results");
+		System.out.println("Value is : " + jsonAttributeValue);
+		Assert.assertEquals(false, jsonAttributeValue);
 	}
 	
 	@Test(description = "Nested json attributes validation", priority = 6)
-	public void nestedJsonAttributes() {
+	public void nestedJsonAttributes() throws InterruptedException {
 		JsonPath jsonPath = response.jsonPath();
-		String totalCount = jsonPath.get("items[0].owner.type");
-		System.out.println("Value is : " + totalCount);
-		Assert.assertEquals("User", totalCount);
+		String nestedJsonValue = jsonPath.get("items[0].owner.type");
+		System.out.println("Nested Json Value is : " + nestedJsonValue);
+		Assert.assertEquals("User", nestedJsonValue);
 	}
 	
 }
